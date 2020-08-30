37213d8560cef9227192e218cdaed680378c0c4a
breewarren <ambreea.warren@gmail.com>
2020-08-30 17:06:34 -0500

    finished in record time

--- lib/lifter.rb
+++ lib/lifter.rb
@@ -1,8 +1,37 @@
 class Lifter
   attr_reader :name, :lift_total
 
+  @@all = []
+
   def initialize(name, lift_total)
     @name = name
     @lift_total = lift_total
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def memberships
+    Membership.all.select {|membership| membership.lifter == self}
+  end
+
+  def gyms
+    memberships.map {|membership| membership.gym}
+  end
+
+  def self.lift_total
+    lift_totals = self.all.sum {|lifter| lifter.lift_total}
+    lift_totals / self.all.count
+  end
+
+  def membership_cost
+    memberships.sum {|membership| membership.cost}
+  end
+
+  def new_lifter(cost, gym)
+    Membership.new(cost, self, gym)
+  end
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,9 +1,40 @@
-# You don't need to require any of the files in lib or pry.
-# We've done it for you here.
+
 require_relative '../config/environment.rb'
 
-# test code goes here
+# Gym <-> Membership <-> Lifter
+
+#Base Instances
+g = Gym.new("Equinox")
+l = Lifter.new("Bree", 967)
+
+#Association Instance
+m = Membership.new(186, l, g)
+
+#Additional Instances
+g2 = Gym.new("Yogaworks")
+l2 = Lifter.new("Ambreea", 475)
+m2 = Membership.new(74, l, g2)
+
 
 binding.pry
 
 puts "Gains!"
+
+
+#Lifter
+# Get a list of all lifters - DONE
+# Get a list of all the memberships that a specific lifter has - DONE
+# Get a list of all the gyms that a specific lifter has memberships to - DONE
+# Get the average lift total of all lifters - DONE
+# Get the total cost of a specific lifter's gym memberships - DONE
+# Given a gym and a membership cost, sign a specific lifter up for a new gym - DONE
+
+# Membership
+# Get a list of all memberships - DONE
+
+# Gym
+# Get a list of all gyms - DONE
+# Get a list of all memberships at a specific gym - DONE
+# Get a list of all the lifters that have a membership to a specific gym - DONE
+# Get a list of the names of all lifters that have a membership to that gym - DONE
+# Get the combined lift total of every lifter has a membership to that gym - DONE
