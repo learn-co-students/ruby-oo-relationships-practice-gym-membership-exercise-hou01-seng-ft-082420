bc092fba183be46565f8d41abf2bc9a7dee510c4
paytongray18 <paytongray18@gmail.com>
2020-09-01 14:10:54 -0500

    Done.

--- lib/gym.rb
+++ lib/gym.rb
@@ -1,7 +1,34 @@
 class Gym
+  
   attr_reader :name
 
+  @@all = []
   def initialize(name)
     @name = name
+
+    self.class.all << self
   end
+
+  def membership
+    Membership.all.select {|m| m.gym == self}
+    #self.all.select 
+  end
+
+  def lifters
+    membership.collect {|n| n.name}
+  end
+
+  def lifter_names
+    membership.collect {|n| n.name}
+  end
+
+  def self.all
+    @@all
+  end
+
+  def total
+    membership.collect {|n| n.name.lift_total}.sum
+  end
+
+
 end
--- lib/lifter.rb
+++ lib/lifter.rb
@@ -1,8 +1,39 @@
+require 'pry'
 class Lifter
   attr_reader :name, :lift_total
 
+  @@all = []
   def initialize(name, lift_total)
     @name = name
     @lift_total = lift_total
+
+    self.class.all << self
   end
+
+  def membership
+    Membership.all.select {|m| m.name == self}
+  end
+
+  def gyms
+    (Membership.all.select {|g| g.name == self}).collect{|g| g.gym.name}
+  end
+
+  def self.lift_total
+    total = self.all.sum {|lt| lt.lift_total}
+    total / self.all.count
+  end
+
+  def self.all
+    @@all
+  end
+
+  def gym_cost
+    membership.collect {|gc| gc.cost}.sum
+  end
+
+  def new_gym(gym,cost)
+    Membership.new(gym,self,cost)
+  end
+
+
 end
