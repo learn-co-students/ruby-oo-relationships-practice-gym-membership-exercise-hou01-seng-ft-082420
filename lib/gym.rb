25e2b2c8ab35a54b94cf9978fc272cbfd5c02696
Far.k <farzana.mkarim@gmail.com>
2020-09-04 20:57:12 -0500

    Done.

--- lib/gym.rb
+++ lib/gym.rb
@@ -1,7 +1,37 @@
 class Gym
   attr_reader :name
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def memberships
+    Membership.all.select do |mem|
+      mem.gym == self
+    end
+  end
+
+  def lifters
+    self.memberships.collect do |mem|
+        mem.lifter
+    end.uniq  
+  end
+
+  def names
+    self.lifters.collect do |lifter|
+      lifter.name
+    end
+  end
+
+  def total_lift
+    self.lifters.sum do |lifter|
+      lifter.lift_total
+    end
   end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -3,7 +3,20 @@
 require_relative '../config/environment.rb'
 
 # test code goes here
+gym1 = Gym.new("24 Hrs")
+gym2 = Gym.new("Equinox")
+gym3 = Gym.new("Lifetime")
 
+l1 = Lifter.new("Vince", 80)
+l2 = Lifter.new("Kelsey", 60)
+l3 = Lifter.new("Kayla", 40)
+
+mem1 = Membership.new(100, l1, gym1)
+mem2 = Membership.new(200, l2, gym2)
+mem3 = Membership.new(300, l3, gym3)
+mem4 = Membership.new(100, l3, gym2)
+mem5 = Membership.new(300, l1, gym3)
+mem6 = Membership.new(300, l1, gym3)
 binding.pry
 
 puts "Gains!"
